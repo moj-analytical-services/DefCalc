fe8d6a3df077a912bed11610e8daa887f87bef0c
dwmoj <daniel.white@justice.gov.uk>
2019-10-24 16:16:45 +0000

    UI improvements

--- idx.R
+++ idx.R
@@ -1,8 +1,8 @@
 #Necessary packages
 library(tidyverse) #dplyr & readr & stringr (plus other functionalities)
 #library(openxlsx) #read in/manipulate Excel files
-library(DT) #output table; better functionality than shiny's tables
 library(s3tools) #importing data from AWS
+library(DT) #required to produce output table
 
 #Download cleaned OBR data .xlsx (not currently in use)
 #download_file_from_s3("alpha-sandbox/obr.xlsx", "obr.xlsx", overwrite = TRUE)
@@ -23,13 +23,20 @@ index_obr_fy <- data.frame(index_obr_fy, row.names = 1)
 
 #selects index drop down options needed for UI (i.e. removes duplicates)
 index_options <- index_obr_all %>% select(starts_with("yoy_"))
-colnames(index_options) <- substring(colnames(index_options),5)
-#replace '.' with " " and ".." with '(' or ')'
-#str_replace(colnames(index_options), "\\.", " ")
-#str_replace(colnames(index_options), "\\..", " ")
+colnames(index_options) <- substring(colnames(index_options), 5)
+#replace '.' with blank space in column names, for UI aesthetic purposes
+colnames(index_options) <- str_replace_all(colnames(index_options), "[.]", " ")
 
 #alters all column names in dataframes; necessary to match drop down with dataframe YoY column names
-colnames(index_obr_all) <- substring(colnames(index_obr_all),5)
-colnames(index_obr_qtr) <- substring(colnames(index_obr_qtr),5)
-colnames(index_obr_pa) <- substring(colnames(index_obr_pa),5)
-colnames(index_obr_fy) <- substring(colnames(index_obr_fy),5)
+colnames(index_obr_all) <- colnames(index_obr_all) %>%
+  substring(5) %>%
+  str_replace_all("[.]", " ")
+colnames(index_obr_qtr) <- colnames(index_obr_qtr) %>%
+  substring(5) %>%
+  str_replace_all("[.]", " ")
+colnames(index_obr_pa) <- colnames(index_obr_pa) %>%
+  substring(5) %>%
+  str_replace_all("[.]", " ")
+colnames(index_obr_fy) <- colnames(index_obr_fy) %>%
+  substring(5) %>%
+  str_replace_all("[.]", " ")
