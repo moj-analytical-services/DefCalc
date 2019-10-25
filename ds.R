c55c39e0806b134009651794d9b59d810232dfee
dwmoj <daniel.white@justice.gov.uk>
2019-10-25 17:24:01 +0000

    major datasource updates

--- ds.R
+++ ds.R
@@ -2,7 +2,7 @@
 library(httr)
 library(readxl)
 library(openxlsx)
-library(dplyr)
+library(tidyverse)
 library(s3tools)
 
 #Import online Excel OBR datafile into R, through an indirect URL
@@ -12,34 +12,29 @@ download.file(obr_url$url, destfile = temp_obr_xlsx,mode = "wb")
 obr_xlsx = read_excel(temp_obr_xlsx, sheet = "1.7") #1.7 is current tab name for Inflation tab
 
 #Clean data to prepare for app
-#removes empty first column and removes all extra rows
-obr_xlsx <- obr_xlsx[2:18] #removes column 1 (blank)
-obr_xlsx <- obr_xlsx %>% slice(4:100) #removes extra rows
-#renames all columns
-#year on year (% change)
-names(obr_xlsx)[names(obr_xlsx)=="...2"] <- "yoy_Period" #time period reference
-names(obr_xlsx)[names(obr_xlsx)=="...3"] <- "yoy_Retail Price Index" #Retail Prices Index
-names(obr_xlsx)[names(obr_xlsx)=="...4"] <- "yoy_Retail Price Index excl MIP" #RPI excluding mortgage interest payments
-names(obr_xlsx)[names(obr_xlsx)=="...5"] <- "yoy_Consumer Price Index" #Consumer Price Index
-names(obr_xlsx)[names(obr_xlsx)=="...6"] <- "yoy_Producer Output Prices" #Producer Output Prices
-names(obr_xlsx)[names(obr_xlsx)=="...7"] <- "yoy_Mortgage Interest Payments" #Mortgage Interest Payments
-names(obr_xlsx)[names(obr_xlsx)=="...8"] <- "yoy_Actual Rents for Housing" #Actual rents for housing
-names(obr_xlsx)[names(obr_xlsx)=="...9"] <- "yoy_Consumer Expenditure Deflator" #Consumer expenditure deflator
-names(obr_xlsx)[names(obr_xlsx)=="...10"] <- "yoy_Gross Domestic Product Deflator" #Gross Domestic Product deflator
-#index figures (currently unused due to how the app works)
-names(obr_xlsx)[names(obr_xlsx)=="...11"] <- "index_RPI" #Jan1987=100
-names(obr_xlsx)[names(obr_xlsx)=="...12"] <- "index_RPIX" #Jan1987=100
-names(obr_xlsx)[names(obr_xlsx)=="...13"] <- "index_CPI" #2015=100
-names(obr_xlsx)[names(obr_xlsx)=="...14"] <- "index_POP" #2010=100
-names(obr_xlsx)[names(obr_xlsx)=="...15"] <- "index_MIP" #Jan1987=100
-names(obr_xlsx)[names(obr_xlsx)=="...16"] <- "index_ARH" #2015=100
-names(obr_xlsx)[names(obr_xlsx)=="...17"] <- "index_CExDef" #2016=100
-names(obr_xlsx)[names(obr_xlsx)=="...18"] <- "index_GDPdef" #2016=100
+#removes empty columns based on if they're entirely 'NA'
+obr_xlsx <- obr_xlsx[, colSums(is.na(obr_xlsx)) != nrow(obr_xlsx)]
+#removes extra rows if they contain any 'NA'; Column 1 assumed to be 'Period', so name is 'NA'
+obr_xlsx <- obr_xlsx[complete.cases(obr_xlsx[ , 2:ncol(obr_xlsx)]), ]
+
+#convert first row into column names. Explictly assumes Column 1 is 'Period'.
+colnames(obr_xlsx)<- obr_xlsx[1,] #copy Row 1 to Column names
+obr_xlsx <- obr_xlsx[-1,] #deletes Row 1
+names(obr_xlsx)[1] <- "Period" #creates column name for 'Period'
+
+#renames new column names based on if they are YoY changes, or Indices
+colnames(obr_xlsx) <- make.unique(colnames(obr_xlsx))
+obr_xlsx_yoy <- obr_xlsx %>% select(-ends_with(".1"))
+obr_xlsx_index <- obr_xlsx %>% select(ends_with(".1"))
+colnames(obr_xlsx_index) <- paste("index", colnames(obr_xlsx_yoy[2:ncol(obr_xlsx_yoy)]), sep = "_")
+colnames(obr_xlsx_yoy) <- paste("yoy", colnames(obr_xlsx_yoy), sep = "_")
+obr_xlsx <- cbind.data.frame(obr_xlsx_yoy, obr_xlsx_index)
+names(obr_xlsx)[names(obr_xlsx)=="yoy_Period"] <- "Period" #time period reference
 
 #creates dataframes for data by quarter, annual and financial year splits flexibly (i.e. auto-updates with new spreadsheet)
-obr_xlsx_qtr <- filter(obr_xlsx, grepl("Q", obr_xlsx$yoy_Period)) #quarterly
-obr_xlsx_pa <- filter(obr_xlsx, nchar(obr_xlsx$yoy_Period, type = "chars") == 4, TRUE) #calendar
-obr_xlsx_fy <- filter(obr_xlsx, grepl("/", obr_xlsx$yoy_Period))  #financial
+obr_xlsx_qtr <- filter(obr_xlsx, grepl("Q", obr_xlsx$Period)) #quarterly
+obr_xlsx_pa <- filter(obr_xlsx, nchar(obr_xlsx$Period, type = "chars") == 4, TRUE) #calendar
+obr_xlsx_fy <- filter(obr_xlsx, grepl("/", obr_xlsx$Period))  #financial
 
 #Adds row names to all dataframes (doing this at the start doesn't copy them to new dataframes)
 obr_xlsx <- data.frame(obr_xlsx, row.names = 1)
