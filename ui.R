0538a661d52f69e0940b60222cf663455dbd47a5
dwmoj <54888964+dwmoj@users.noreply.github.com>
2019-10-28 17:12:15 +0000

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,44 +8,55 @@
 #
 
 library(shiny)
+library(shinyWidgets)
 
 source("./idx.R")
 
 # UI for Indexation app
 ui = navbarPage("Indexation Tool",
                 
-                #guidance tab
-                tabPanel("Guidance",
-                         mainPanel(
-                           h1("Guidance"), h3("Info"),
-                           h2("Indices"), h3("Info"),
-                           h2("Deflator Calculator"), h3("Info"),
-                           h2("Placeholder"), h3("Info")
-                         )
-                ),
+    #guidance tab
+    tabPanel("Guidance",
+       mainPanel(
+        h1("Guidance"), h3("Info"),
+        h2("Indices"), h3("Info"),
+        h2("Deflator Calculator"), h3("Info"),
+        h2("Placeholder"), h3("Info")
+       )
+    ),
                 
-                #indices tab
-                tabPanel("Indices",
-                         sidebarPanel(
-                           #dropdown menus: indices = different indices available; period = frequency of data; baseyear = year chosen as 'Index=100'
-                           selectInput(inputId = "indices", label = "Index", choices = colnames(index_options)),
-                           selectInput(inputId = "period", label = "Period Reference", choices = c("Quarterly", "Calendar Year", "Financial Year")),
-                           uiOutput("base")
-                         ),
-                         mainPanel(dataTableOutput("indextable")
-                         )
-                ),
+    #indices tab (inputs pre-fixed with: 'i_')
+    tabPanel("Indices",
+       sidebarPanel(
+       #dropdown menus: indices = different indices available; period = frequency of data; baseyear = chosen base year
+        selectInput(inputId = "i_indices", label = "Index", choices = colnames(index_options)),
+        selectInput(inputId = "i_period", label = "Period Reference", choices = c("Quarterly", "Calendar Year", "Financial Year")),
+        uiOutput("i_base")
+       ),
+       mainPanel(dataTableOutput("i_indextable")
+       )
+    ),
                 
-                #deflator calculator tab
-                navbarMenu("Deflator Calculator",
-                           #sub-tab for user to input data
-                           tabPanel("Input"
-                                    
-                           ),
-                           #sub-tab to display output data once user's input data is transformed
-                           tabPanel("Output"
-                                    
-                           )
-                           
-                )
+    #deflator calculator tab (inputs pre-fixed with 'dc_')
+    tabPanel("Deflator Calculator",
+      sidebarPanel(
+      #dropdown menus: indices = different indices available; period = frequency of data; realnom = real or nominal adjustment; slideryears = years covered in input data
+      selectInput(inputId = "dc_indices", label = "Index", choices = colnames(index_options)),
+      selectInput(inputId = "dc_realnom", label = "Conversion: Real/Nominal", choices = c("Real to Nominal", "Nominal to Real")),
+      selectInput(inputId = "dc_period", label = "Period Reference", choices = c("Quarterly", "Calendar Year", "Financial Year")),
+      uiOutput("dc_slideryears"),
+      uiOutput("dc_from"),
+      uiOutput("dc_to")
+      ),
+      
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  #input tab for user data
+                  tabPanel("Input"),
+                  
+                  #output tab to display transformed data
+                  tabPanel("Output")
+      )
+    )
+    )
 )
