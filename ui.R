ed76ad4440ca97c8737bb0554171a32c0a9843d5
dwmoj <daniel.white@justice.gov.uk>
2019-10-30 09:17:29 +0000

    start of rhandsontable code

--- ui.R
+++ ui.R
@@ -1,51 +1,82 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# 
+#  This is the user-interface definition of a Shiny web application. You can
+#  run the application by clicking 'Run App' above.
+# 
+#  Find out more about building applications with Shiny here:
+# 
+#     http://shiny.rstudio.com/
+# 
 
 library(shiny)
+library(shinyWidgets)
 
 source("./idx.R")
 
 # UI for Indexation app
 ui = navbarPage("Indexation Tool",
                 
-                #guidance tab
-                tabPanel("Guidance",
-                         mainPanel(
-                           h1("Guidance"), h3("Info"),
-                           h2("Indices"), h3("Info"),
-                           h2("Deflator Calculator"), h3("Info"),
-                           h2("Placeholder"), h3("Info")
-                         )
-                ),
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INDICES TABLE | START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 
-                #indices tab
-                tabPanel("Indices",
-                         sidebarPanel(
-                           #dropdown menus: indices = different indices available; period = frequency of data; baseyear = year chosen as 'Index=100'
-                           selectInput(inputId = "indices", label = "Index", choices = colnames(index_options)),
-                           selectInput(inputId = "period", label = "Period Reference", choices = c("Quarterly", "Calendar Year", "Financial Year")),
-                           uiOutput("base")
-                         ),
-                         mainPanel(dataTableOutput("indextable")
-                         )
-                ),
+# all variables pre-fixed with 'i_' to prevent duplication with other outputs                
                 
-                #deflator calculator tab
-                navbarMenu("Deflator Calculator",
-                           #sub-tab for user to input data
-                           tabPanel("Input"
-                                    
-                           ),
-                           #sub-tab to display output data once user's input data is transformed
-                           tabPanel("Output"
-                                    
-                           )
-                           
-                )
+# guidance tab
+  tabPanel("Guidance",
+         mainPanel(
+           h1("Guidance"), 
+            h4("Please ensure the guidance is read thoroughly to ensure the tool is not misused."),
+           h2("Indices"), 
+            h4("Provides information on commonly used, publicly published indices"),
+           h2("Deflator Calculator"),
+            h4("Allows one to convert monetary figures from real values to nominal values, and vice versa."),
+           h2("Placeholder"), 
+            h4("Info")
+         )
+  ),
+                
+  # indices tab (inputs pre-fixed with: 'i_')
+  tabPanel("Indices",
+     sidebarPanel(
+       # dropdown menus: indices = different indices available; period = frequency of data; baseyear = chosen base year
+       selectInput(inputId = "i_indices", label = "Index", choices = colnames(index_options)),
+       selectInput(inputId = "i_period", label = "Period Reference", choices = c("Quarterly", "Calendar Year", "Financial Year")),
+       uiOutput("i_base")
+     ),
+   mainPanel(dataTableOutput("i_indextable")
+   )
+  ),
+                
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INDICES TABLE | END ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEFLATOR CALCULATOR | START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+                
+# all variables pre-fixed with 'dc_' to prevent duplication with other outputs                
+                
+tabPanel("Deflator Calculator",
+  sidebarPanel(
+     # dropdown menus: indices = different indices available; period = frequency of data; realnom = real or nominal adjustment; slideryears = years covered in input data
+     selectInput(inputId = "dc_indices", label = "Index", choices = colnames(index_options)),
+     selectInput(inputId = "dc_realnom", label = "Conversion: Real/Nominal", choices = c("Real to Nominal", "Nominal to Real")),
+     uiOutput("dc_fromto"),
+     selectInput(inputId = "dc_period", label = "Period Reference", choices = c("Quarterly", "Calendar Year", "Financial Year")),
+     uiOutput("dc_slideryears")
+   ),
+                         
+  mainPanel(
+     tabsetPanel(type = "tabs",
+      # input tab for user data
+      tabPanel("Input",
+          rHandsontableOutput("hot")
+      ),
+                                   
+      # output tab to display transformed data
+      tabPanel("Output",
+          rHandsontableOutput("cold")
+     )
+  )
+)
+
+)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DEFLATOR CALCULATOR | END ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 )
