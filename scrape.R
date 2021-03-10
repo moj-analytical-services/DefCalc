4dfd6e64e3fcd7ea56d8a61d4836bd2746d65c2a
dwmoj <54888964+dwmoj@users.noreply.github.com>
2021-03-10 12:32:26 +0000

    update for HMT

--- scrape.R
+++ scrape.R
@@ -1,33 +1,70 @@
-### Note that this will not be used by the app. It is included for informative purposes only.
-### It is built to run as a scraping script as and when the below code is run.
-### airflow-defcalc has a copy of this script that automates the scraping (daily).
-
 # Necessary packages
 library(httr)
 library(readxl) 
 library(openxlsx) # read in/manipulate Excel files
 library(tidyverse) # dplyr & readr & stringr (plus other functionalities)
 library(s3tools) # importing data from AWS
-library(RCurl) # get information relating to a URL. 
+library(RCurl) # get information relating to a URL
+library(xml2) # required for rvest
+library(rvest) # get information relating to a URL
+library(lubridate) # to create the list of dates to check against for URLs
+
+library(reticulate) # required for auto-email updates
+
+print("Print Statement: Packages loaded")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ WEB SCRAPING FOR FILE | START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Create a list of dates between now and 12 months prior. 
-date_today <- as.POSIXlt(Sys.Date())
-date_back <- date_today
-date_back$mon <- date_back$mon - 12
-latest_url <- format(seq(date_back, date_today, "month"), "%B-%Y") %>% as.data.frame(col.names = "Dates")
+# Create a list of dates between now and 12 months prior.
+date_end <- floor_date(as.POSIXlt(Sys.Date()), "months")
+date_start <- date_end
+date_start$mon <- date_end$mon - 12
+date_length <- interval(date_start, date_end)/months(1)
+date_list <- as.data.frame(date_start + months(0:(date_length)))
+latest_url <- format(date_list, "%B-%Y")
 names(latest_url) <- "Date"
+
+print(latest_url["Date"])
+print("Print Statement: URL dataframe structure created")
 
 # Gives the structure of the URL, which we'll use and replace 'march-2019' with the series of month-years created above.
 ourl <- "https://obr.uk/download/march-2019-economic-and-fiscal-outlook-supplementary-economy-tables/"
+osearch <- "https://obr.uk/efo/economic-and-fiscal-outlook-march-2019/"
+obr <- "https://obr.uk/publications"
+
+print("Print Statement: URL structure provided")
 
 # Create URLs and test if they exist, create column of URLS and replace with the months_years from above. Check if each URL exists. 
 latest_url$urls <- ourl
 sub = function(x,y,z)gsub(x,y,z)
 latest_url$urls <- mapply(sub, "march-2019", latest_url$Date, latest_url$urls)
-latest_url$exist <- sapply(latest_url$urls, url.exists)
 latest_url$filename <- paste(paste('Economy_supplementary_tables', latest_url$Date, "EFO", sep = "_"),  "xlsx", sep = ".")
 latest_url$filename <- gsub("-", "_", latest_url$filename)
+
+latest_url$search <- osearch
+latest_url$search <- mapply(sub, "march-2019", latest_url$Date, latest_url$search)
+
+print("Print Statement: URL list created")
+
+# Scrape 'obr' to find list of hrefs to match against 'ourl'
+obr_html <- read_html(obr)
+obr_hrefs <- html_attr(html_nodes(obr_html, "a"), "href")
+
+print("Print Statement: HTML download links identified")
+
+# Match obr_hrefs against latest_url$urls
+for (i in 1:nrow(latest_url)) {
+  
+  if (any(grep(latest_url[i, "search"], obr_hrefs, ignore.case = TRUE) != 0)) {
+    
+    latest_url[i, "exist"] <- TRUE
+    }
+    else { 
+    latest_url[i, "exist"] <- FALSE
+    }
+  
+}
+
+print("Print Statement: Latest EFO version identified")
 
 # Test if files are in S3 and if they're not then indicate the URL that needs to be downloaded.
 s3files <- s3tools::list_files_in_buckets('alpha-app-defcalc')
@@ -49,6 +86,8 @@ latest_url$updatereq <- ifelse((latest_url$exist == TRUE & latest_url$s3 == 0 & 
 latest_url$weblink <- paste('https://obr.uk/efo/economic-and-fiscal-outlook', latest_url$Date,sep = "-")
 latest_filename <- latest_url$filename[which(latest_url$latest == 1)]
 
+print("Print Statement: Check latest URL against S3 buckets complete")
+
 # Pick out, of the latest_url dataframe, the url that needs to be downloaded and the corresponding filename.
 updateurl <- ifelse(length(latest_url$urls[latest_url$updatereq == 1]) == 0, latest_url$urls[latest_url$s3 == 1 & latest_url$latest == 1], latest_url$urls[latest_url$updatereq == 1])
 
@@ -58,6 +97,48 @@ updateurl <- ifelse(length(latest_url$urls[latest_url$updatereq == 1]) == 0, lat
 
 # Only run rest of code if updatereq has a non-zero value
 if (is_empty(which(latest_url$updatereq == 1) == 0) == FALSE) {
+  
+  print("Print Statement: Update required: code running...")
+  
+  ### Auto-email code
+  #R wrapper for send_contributor_email_py
+  send_contributor_email = function(contrib_email, QA_table){
+    
+    # Read in python email function
+    reticulate::source_python("./gov_notify.py")
+    
+    # Get api key
+    api_key_raw = s3tools::read_using(
+      read.delim,
+      s3_path = 'alpha-app-defcalc/indexationtool_api.txt',
+      header = FALSE,
+      sep = "",
+      quote = ""
+    )
+    
+    api_key = as.character(api_key_raw[[1]])
+    
+    return(send_contributor_email_py(contrib_email,
+                                     "",
+                                     api_key))
+    
+  }
+  
+  jason_table = jsonlite::toJSON("")
+  
+  #Loop created to cycle through all emails (not ideal, but could not generate a list in below function)
+  email_list <- c("daniel.white@justice.gov.uk",
+                  "james.leicester@justice.gov.uk",
+                  "michelle.tenwick@justice.gov.uk",
+                  "megan.chambers@justice.gov.uk")
+  
+  # Must be added as members of the team on 'gov.uk/notify'
+  for (i in 1:length(email_list)) {
+    
+  send_contributor_email(email_list[i],
+                          jason_table)
+  }
+  
   
   # Import online Excel OBR datafile into R, through an indirect URL
   ourl = updateurl
@@ -96,18 +177,19 @@ if (is_empty(which(latest_url$updatereq == 1) == 0) == FALSE) {
   # Removes empty columns based on if they're entirely 'NA'
   obr_xlsx_inf <- obr_xlsx_inf[, colSums(is.na(obr_xlsx_inf)) != nrow(obr_xlsx_inf)]
   obr_xlsx_awe <- obr_xlsx_awe[, colSums(is.na(obr_xlsx_awe)) != nrow(obr_xlsx_awe)]
-  # Removes extra rows if they contain any 'NA'; Column 1 assumed to be 'Period', so name is 'NA'
-  obr_xlsx_inf <- obr_xlsx_inf[complete.cases(obr_xlsx_inf[ , 2:ncol(obr_xlsx_inf)]), ]
-  obr_xlsx_awe <- obr_xlsx_awe[complete.cases(obr_xlsx_awe[ , 2:ncol(obr_xlsx_awe)]), ]
+  obr_xlsx_inf <- obr_xlsx_inf[-c(1,2),] # deletes Row 1 & 2
+  obr_xlsx_awe <- obr_xlsx_awe[-1,] # deletes Row 1
   
   # Convert first row into column names. Explictly assumes Column 1 is 'Period'.
   colnames(obr_xlsx_inf)<- obr_xlsx_inf[1,] # copy Row 1 to Column names
-  obr_xlsx_inf <- obr_xlsx_inf[-1,] # deletes Row 1
   names(obr_xlsx_inf)[1] <- "Period" # creates column name for 'Period'
   
   colnames(obr_xlsx_awe)<- obr_xlsx_awe[1,] # copy Row 1 to Column names
-  obr_xlsx_awe <- obr_xlsx_awe[-1,] # deletes Row 1
   names(obr_xlsx_awe)[1] <- "Period" # creates column name for 'Period'
+  
+  # Removes extra rows if they contain any 'NA'; Row 2 exempt
+  obr_xlsx_inf <- obr_xlsx_inf[complete.cases(obr_xlsx_inf[ , 1:ncol(obr_xlsx_inf)]), ]
+  obr_xlsx_awe <- obr_xlsx_awe[complete.cases(obr_xlsx_awe[ , 1:ncol(obr_xlsx_awe)]), ]
   
   # Removes extra columns in AWE dataset that aren't related to the index of interest
   awe_columns <- grep("average earnings", colnames(obr_xlsx_awe), ignore.case = TRUE)
@@ -132,6 +214,7 @@ if (is_empty(which(latest_url$updatereq == 1) == 0) == FALSE) {
   # Combines Inflation & AWE datasets
   obr_xlsx <- cbind.data.frame(obr_xlsx_inf_yoy, obr_xlsx_awe_yoy, obr_xlsx_inf_index, obr_xlsx_awe_index)
   names(obr_xlsx)[names(obr_xlsx)=="yoy_Period"] <- "Period" #time period reference
+  obr_xlsx$Period <- str_replace_all(obr_xlsx$Period, "-", "/")
   
   # Creates dataframes for data by quarter, annual and financial year splits flexibly (i.e. auto-updates with new spreadsheet)
   obr_xlsx_qtr <- filter(obr_xlsx, grepl("Q", obr_xlsx$Period)) #quarterly
@@ -167,8 +250,8 @@ if (is_empty(which(latest_url$updatereq == 1) == 0) == FALSE) {
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FILE SAVE | START ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   # Save dataframes as both .xlsx and .csv files (only .csv is used in app)
-  saveWorkbook(obr, temp_obr_xlsx, overwrite = TRUE)
-  write_file_to_s3("obr.xlsx", paste0("alpha-app-defcalc/", temp_obr_xlsx), overwrite = TRUE)
+  # saveWorkbook(obr, temp_obr_xlsx, overwrite = TRUE)
+  # write_file_to_s3("obr.xlsx", paste0("alpha-app-defcalc/", temp_obr_xlsx), overwrite = TRUE)
   
   # Save dataframes as both .xlsx and .csv files (only .csv is used in app)
   saveWorkbook(obr, "obr.xlsx", overwrite = TRUE)
@@ -202,6 +285,8 @@ if (is_empty(which(latest_url$updatereq == 1) == 0) == FALSE) {
   file.remove("obr_qtr.csv")
   file.remove("obr_pa.csv")
   file.remove("latest_url.csv")
+  
+  print("Print Statement: Update completed")
   
   
 }
